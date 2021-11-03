5c4336a9946a239c92f640ef9236dc41dcf0ccfb
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-11-03 22:42:23 +0200

    Testing libary ddl

---
+++ DLLImportLibary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GoToCSharp
+{
+    public class DLLImportLibary
+    {
+        [DllImport("User32.dll")]
+        public static extern int MessageBoxA(IntPtr intPtr, string text, string caption, uint type);
+
+
+        [DllImport("User32.dll")]
+        public static extern int SetForegroundWindow(IntPtr intPtr);
+    }
+}
